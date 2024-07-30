337bb5172606e0a19a3cacaf1a3d3028cdeeeab7
Adrian Rumpold <a.rumpold@gmail.com>
2024-07-30 12:24:30 +0200

    fix: Replace Looker Studio template data sources

--- src/gh_project_metrics/gcp.py
+++ src/gh_project_metrics/gcp.py
@@ -16,10 +16,18 @@ def get_gcp_project_id(args: argparse.Namespace) -> str | None:
 def create_lookerstudio_report_url(
     template_report_id: str, project_name: str, gcp_project_id: str, bq_dataset: str
 ) -> str:
+    tables = ["referrers", "views", "stars", "downloads", "views", "clones"]
     params = {
         "c.reportId": template_report_id,
         "r.reportName": f"Project metrics for {project_name}",
-        "ds.*.projectId": gcp_project_id,
-        "ds.*.datasetId": bq_dataset,
     }
+
+    for table in tables:
+        params |= {
+            f"ds.{table}.connector": "bigQuery",
+            f"ds.{table}.type": "TABLE",
+            f"ds.{table}.projectId": gcp_project_id,
+            f"ds.{table}.datasetId": bq_dataset,
+            f"ds.{table}.tableId": table,
+        }
     return "https://lookerstudio.google.com/reporting/create?" + urllib.parse.urlencode(params)
