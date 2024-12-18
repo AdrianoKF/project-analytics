35fb8dec211b8da6c3ef25dfee532bb96a46e61d
Adrian Rumpold <a.rumpold@gmail.com>
2024-12-18 09:22:07 +0100

    feat: Two-way Plotly I/O manager

--- src/gh_project_metrics/dagster/io_managers.py
+++ src/gh_project_metrics/dagster/io_managers.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 import plotly.graph_objects as go
-from dagster import ConfigurableIOManager
+import plotly.io as pio
+from dagster import ConfigurableIOManager, OutputContext
 
 from gh_project_metrics.dagster.utils import parse_partition_key
 from gh_project_metrics.plotting import write_plot
@@ -17,14 +18,25 @@ class PlotlyIOManager(ConfigurableIOManager):
     width: int = 1200  # Width of the PNG images in pixels
     height: int = 800  # Height of the PNG images in pixels
 
-    def load_input(self, context):
-        raise NotImplementedError("Loading not supported")
+    def _partition_path(self, context: OutputContext) -> Path:
+        partition = parse_partition_key(context.partition_key)
+        return (
+            Path(self.root_directory)
+            / partition.project
+            / partition.date.strftime("%Y-%m-%d")
+            / Path(*context.asset_key.path)
+        )
+
+    def load_input(self, context) -> dict[str, go.Figure]:
+        pdir = self._partition_path(context)
+        plots = {}
+        for plot_file in pdir.glob("*.json"):
+            name = plot_file.stem
+            plots[name] = pio.from_json(plot_file.read_text())
+        return plots
 
     def handle_output(self, context, obj: dict[str, go.Figure]):
-        partition = parse_partition_key(context.partition_key)
-
-        pdir = Path(self.root_directory) / partition.project / partition.date.strftime("%Y-%m-%d")
+        pdir = self._partition_path(context)
         pdir.mkdir(parents=True, exist_ok=True)
-
         for name, plot in obj.items():
             write_plot(plot, pdir, name, width=self.width, height=self.height)
